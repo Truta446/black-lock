ae4b015dfc4512e1640722772726c22c44cb321f
Juan Versolato Lopes <juan.versolato@outlook.com>
2021-06-09 17:35:38 -0400

    Chore: Create new project.

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: true,
+  appDomain: 'localhost:4200',
+  useEmulators: false,
+  firebase: {
+    apiKey: "AIzaSyBNPSJ6tot23_737KyRbnPuJOAcoZ8UIAY",
+    authDomain: "black-lock-52770.firebaseapp.com",
+    projectId: "black-lock-52770",
+    storageBucket: "black-lock-52770.appspot.com",
+    messagingSenderId: "285548849912",
+    appId: "1:285548849912:web:54bade63cc4b8ded50290d",
+    measurementId: "G-JQS5LQVEQJ"
+  },
+};
