0f92f9d84a9d46b3ad89aeeae15a56ab2c020cb7
Juan Versolato Lopes <juan.versolato@outlook.com>
2021-06-10 21:51:21 -0400

    CHore: Pass tests.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { DashboardComponent } from './dashboard/dashboard.component'
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component'
-import { AuthGuard } from './auth/auth.guard'
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
   {
